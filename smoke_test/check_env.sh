5bc055aeb02f9a760e1ff2ae002106b0478e4b3a
YAO Matrix <yaoweifeng0301@126.com>
2017-07-12 19:25:02 +0800

    add smoke test scripts
    Signed-off-by: YAO Matrix <yaoweifeng0301@126.com>

--- smoke_test/check_env.sh
+++ smoke_test/check_env.sh
@@ -2,7 +2,8 @@
 
 # From Intel Cooperation
 
-# check cpu name
+echo "=====================OS Environment==============="
+# check CPU
 CPU_NAME=`cat /proc/cpuinfo |grep "model name"|awk -F ':' '{print $2}'|uniq`
 echo "-----------------CPU NAME -------------------"
 echo "$CPU_NAME"
@@ -12,9 +13,9 @@ RAM_SIZE=`free -h`
 echo "----------------MEMORY SIZE -----------------"
 echo "$RAM_SIZE"
 
-echo "-------------MEMORY CLOCK SPEED --------------"
-RAM_CLOCK=`dmidecode -t memory|grep -ie HZ`
-echo "$RAM_CLOCK"
+# echo "-------------MEMORY CLOCK SPEED --------------"
+# RAM_CLOCK=`dmidecode -t memory|grep -ie HZ`
+# echo "$RAM_CLOCK"
 
 # check HT
 HT=`lscpu|grep "per core"|awk -F ':' '{print $2}'|sed 's/^ *\| *$//g'`
@@ -36,6 +37,26 @@ else
 	echo "TURBO:On"
 fi
 
+# check CPU power governor
+echo "-------------------POWER GOVERNOR---------------------"
+POWER_GOVERNOR=`cpupower frequency-info | grep \"*\" | awk -F '\"' '{print $2}'`
+echo $POWER_GOVERNOR
+
+# check NUMA and MCDRAM configuration
+# You should $yum install hwloc
+echo "-------------------NUMA and MCDRAM mode---------------------"
+CLUSTER_MODE=`hwloc-dump-hwdata |grep -i Cluster\ Mode: | awk -F ' ' '{print $3}'`
+echo "Cluster Mode: "$CLUSTER_MODE
+MEMORY_MODE=`hwloc-dump-hwdata |grep -i Cluster\ Mode: | awk -F ' ' '{print $(NF)}'` 
+echo "Memory Mode: "$MEMORY_MODE
+
+# check OS
+echo "-------------------OS VERSION---------------------"
+OS_VERSION=`uname -a`
+echo $OS_VERSION
+
+echo "===================SOFTWARE SETTINGS===================="
+
 # check threads env
 echo "-------------------THREADS ENV---------------------"
 MKL_NUM_THREADS=`env |grep MKL_NUM_THREADS`
@@ -54,30 +75,14 @@ fi
 
 KMP_AFFINITY=`env |grep KMP_AFFINITY`
 if [ "${KMP_AFFINITY}" = "" ]; then
-	echo "KMP_AFFINITY:unset"
+	echo "KMP_AFFINITY:UNSET"
 else
 	echo "$KMP_AFFINITY"
 fi
 
-# check CPU power governor
-echo "-------------------POWER GOVERNOR---------------------"
-POWER_GOVERNOR=`cpupower frequency-info` | grep 
-echo $POWER_GOVERNOR
-if [ "${POWER_GOVERNOR}" = "" ]; then
-	echo "KMP_AFFINITY:unset"
-else
-	echo "$KMP_AFFINITY"
-fi
-
-# check NUMA and MCDRAM configuration
-# You should yum install hwloc
-echo "-------------------NUMA and MCDRAM mode---------------------"
-POWER_GOVERNOR=`hwloc` | grep 
-echo $POWER_GOVERNOR
-if [ "${POWER_GOVERNOR}" = "" ]; then
-	echo "KMP_AFFINITY:unset"
-else
-	echo "$KMP_AFFINITY"
-fi
+# check GCC version
+echo "-------------------GCC VERSION---------------------"
+GCC_VERSION=`gcc --version`
+echo $GCC_VERSION
 
 
