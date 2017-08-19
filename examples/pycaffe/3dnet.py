5e75a2eef97f575ccd715f1d859ca609a59ed203
YAO Matrix <yaoweifeng0301@gmail.com>
2017-08-19 09:26:39 +0800

    add deconv in 3dnet example
    Signed-off-by: YAO Matrix <yaoweifeng0301@gmail.com>

--- examples/pycaffe/3dnet.py
+++ examples/pycaffe/3dnet.py
@@ -3,8 +3,8 @@ from caffe import layers as L, params as P, to_proto
 
 def mynet():
     data, label = L.DummyData(shape=[dict(dim=[8, 1, 28, 28]),
-                                         dict(dim=[8, 1, 1, 1])],
-                                  transform_param=dict(scale=1./255), ntop=2)
+                                     dict(dim=[8, 1, 1, 1])],
+                              transform_param=dict(scale=1./255), ntop=2)
 
     # CAFFE = 1
     # MKL2017 = 3
@@ -13,7 +13,17 @@ def mynet():
     bn1 = L.BatchNorm(conv1, **kwargs)
     relu1 = L.ReLU(bn1, **kwargs)
 
-    return to_proto(relu1)
+    convargs = {'param': [dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=2)],
+                'convolution_param': dict(num_output=64, 
+                                          kernel_size=2,
+                                          stride=2,
+                                          engine=P.Convolution.CAFFE,
+                                          bias_filler=dict(type='constant', value=0),
+                                          weight_filler=dict(type='xavier'))
+               }
+    deconv1 = L.Deconvolution(relu1, **convargs)
+
+    return to_proto(deconv1)
 
 net = mynet()
 
